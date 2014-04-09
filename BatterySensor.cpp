0159a3b4934aaa44b35ce5997d2abae0f8e19989
naomi <naomi@l0l.org.uk>
2014-04-09 13:29:45 +0100

    Jeeport class stuff working correctly

--- BatterySensor.cpp
+++ BatterySensor.cpp
@@ -1,24 +1,20 @@
 #include "BatterySensor.h"
 
 BatterySensor::BatterySensor(byte port) {
-  //jeeport = new Port(port);
+  jeeport = new Port(port);
 }
 
 void BatterySensor::setup() {
-  Port jport(2);
-  jport.mode(INPUT);
-  jport.mode2(OUTPUT);
-  jport.digiWrite(1); // stops excess charge accumulating on cap
-  Serial.println(jport.digiRead());
-  jport.digiWrite(LOW); 
-  Serial.println(jport.digiRead());
+  jeeport->mode(OUTPUT);
+  jeeport->mode2(INPUT);
+  jeeport->digiWrite(HIGH); // stops excess charge accumulating on cap
 }
 
 void BatterySensor::measure(int * readings) {
-  /*jport.digiWrite(LOW);
   readings[0] = 1; // sensor id
-  readings[1] = analogRead(A1);
-  jport.digiWrite(HIGH);
+  jeeport->digiWrite(LOW);
+  readings[1] = jeeport->anaRead();
+  jeeport->digiWrite(HIGH);
   readings[2] = 0; // pad with 0 as no more data*/
 }
 
