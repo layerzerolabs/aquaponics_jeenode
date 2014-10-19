fb8d18b7987f39ecb45a32ddd1ccbbc55c82a4ed
layerzerolabs <info@l0l.org.uk>
2014-10-19 17:56:52 +0100

    further ph sensor cleanup

--- aquaponics_jeenode/pHSensor.h
+++ aquaponics_jeenode/pHSensor.h
@@ -5,15 +5,12 @@ class pHSensor : public Sensor {
   
  public:
     static const int id = 5;
-    static const float vRef = 3.3;
-    static const float opampGain = 5.25;
     char* getName();
     pHSensor(byte port, byte address, byte powerPortNo);
     void setup();
     void measure(int* data);
     
   private:
-    float calcpH(int raw);
     int address;
     Port powerPort;
 };
